9c4ce10a8cb7221d8e25148c31d5839a6325bfb6
Hector Sanjuan <hector@convivencial.org>
2013-05-04 16:41:08 +0200

    Small improvements in install script
    Show usage when any argument is given (like -h)
    
    Break on error

--- install.sh
+++ install.sh
@@ -16,6 +16,20 @@
 #--------------------------------------------------------------------------- #
 
 
+function error {
+    echo "There was an error performing the installation!"
+    exit 1
+}
+
+if [ -n "$1" ] ; then
+    echo "Usage: ./install.sh"
+    echo
+    msg="If \$ONE_LOCATION is defined, it will install the files for a  \"self-contained\" OpenNebula installation, under the indicated directory. Otherwise, the installation will be performed in the regular OpenNebula system-wide locations."
+    echo $msg | fmt
+    echo
+    exit 0
+fi
+
 if [ -z "$ONE_LOCATION" ] ; then
     echo "Installing system wide..."
     ONE_LIB_DIR="/usr/lib/one"
@@ -23,7 +37,7 @@ if [ -z "$ONE_LOCATION" ] ; then
     ONE_VAR_DIR="/var/lib/one"
     ONE_SHARE_DIR="/usr/share/one"
 else
-    echo "Installing self-contained"
+    echo "Installing self-contained under $ONE_LOCATION"
     ONE_LIB_DIR=$ONE_LOCATION/lib
     ONE_ETC_DIR=$ONE_LOCATION/etc
     ONE_VAR_DIR=$ONE_LOCATION/var
@@ -41,19 +55,26 @@ SUNSTONE_USER_PLUGINS=$ONE_LIB_DIR/sunstone/public/js/user-plugins/
 #copy remotes in var/
 [[ -d $ONE_VMM_DIR_VBOX ]] || mkdir -p $ONE_VMM_DIR_VBOX
 cp -v remotes/vmm/vbox/* $ONE_VMM_DIR_VBOX
+[[ $? -eq 0 ]] || error
+
 [[ -d $ONE_IM_DIR_VBOX ]] || mkdir -p $ONE_IM_DIR_VBOX
 cp -v remotes/im/vbox.d/* $ONE_IM_DIR_VBOX
+[[ $? -eq 0 ]] || error
 
 # prepare im_vbox config folder
 [[ -d $ONE_ETC_DIR_IM_VBOX ]] || mkdir -p $ONE_ETC_DIR_IM_VBOX
 
 # copy vmm_exec_vbox.conf file with default attributes vbox driver
 cp -v etc/vmm_exec/vmm_exec_vbox.conf $ONE_ETC_DIR_VMM_VBOX
+[[ $? -eq 0 ]] || error
 
 # copy sunstone plugin
 cp -v sunstone/vbox-plugin.js $SUNSTONE_USER_PLUGINS
+[[ $? -eq 0 ]] || error
 
 # install examples
 [[ -d $EXAMPLES_VBOX ]] || mkdir -p $EXAMPLES_VBOX
 cp -v share/examples/vbox/* $EXAMPLES_VBOX
+[[ $? -eq 0 ]] || error
+
 echo "Installation completed successfully!"
